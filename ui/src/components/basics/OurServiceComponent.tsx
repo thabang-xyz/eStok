1d195663975a19a11b427b2b7fcd8bbcedea28e3
Akha Magaqana <akham20@gmail.com>
2024-11-23 00:26:40 +0200

    add text for api

--- ui/src/components/basics/OurServiceComponent.tsx
+++ ui/src/components/basics/OurServiceComponent.tsx
@@ -1,6 +1,9 @@
 const OurServiceComponent = () => {
   return (
     <div className='p-2'>
+      <h1 className='text-brand-primary font-bold text-2xl mb-4 uppercase'>
+        Our Services
+      </h1>
       <p className='text-normal text-gray-500'>
         At eSkot, we offer a comprehensive range of services tailored to
         simplify and enhance your stokvel experience. Whether you're a group
@@ -8,7 +11,7 @@ const OurServiceComponent = () => {
         need to manage and grow your stokvel effortlessly.
       </p>
 
-      <div className='flex flex-wrap'>
+      <div className='flex flex-wrap mt-6'>
         <div className='lg:w-[49%] md:w-full rounded-lg shadow border md:mt-0 xl:p-0 border-brand-primary mx-[0.5%] my-2'>
           <h1 className='text-xl font-bold leading-tight tracking-tight md:text-2xl text-brand-primary p-2'>
             Digital Stokvel Management
@@ -56,47 +59,46 @@ const OurServiceComponent = () => {
                 Generate detailed reports on group finances, including
                 contributions, withdrawals, and overall fund growth.
               </li>
-              <li className='text-normal text-gray-500'></li>
+              <li className='text-normal text-gray-500'>
+                Access visual analytics to help members make informed financial
+                decisions.
+              </li>
             </ul>
           </p>
         </div>
 
         <div className='lg:w-[49%] md:w-full rounded-lg shadow border md:mt-0 xl:p-0 border-brand-primary mx-[0.5%] my-2'>
-          <h1 className='text-xl font-bold leading-tight tracking-tight md:text-2xl text-brand-primary p-2'></h1>
+          <h1 className='text-xl font-bold leading-tight tracking-tight md:text-2xl text-brand-primary p-2'>
+            Dispute Resolution and Governance Tools
+          </h1>
           <p className='p-2'>
             <ul className='space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400'>
-              <li className='text-normal text-gray-500'></li>
-              <li className='text-normal text-gray-500'></li>
+              <li className='text-normal text-gray-500'>
+                Built-in tools to handle member disputes and ensure group
+                governance.
+              </li>
+              <li className='text-normal text-gray-500'>
+                Vote on major group decisions digitally, with transparent
+                outcomes.
+              </li>
             </ul>
           </p>
         </div>
 
         <div className='lg:w-[49%] md:w-full rounded-lg shadow border md:mt-0 xl:p-0 border-brand-primary mx-[0.5%] my-2'>
-          <h1 className='text-xl font-bold leading-tight tracking-tight md:text-2xl text-brand-primary p-2'></h1>
+          <h1 className='text-xl font-bold leading-tight tracking-tight md:text-2xl text-brand-primary p-2'>
+            Customizable Savings and Investment Plans
+          </h1>
           <p className='p-2'>
             <ul className='space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400'>
-              <li className='text-normal text-gray-500'></li>
-              <li className='text-normal text-gray-500'></li>
-            </ul>
-          </p>
-        </div>
-
-        <div className='lg:w-[49%] md:w-full rounded-lg shadow border md:mt-0 xl:p-0 border-brand-primary mx-[0.5%] my-2'>
-          <h1 className='text-xl font-bold leading-tight tracking-tight md:text-2xl text-brand-primary p-2'></h1>
-          <p className='p-2'>
-            <ul className='space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400'>
-              <li className='text-normal text-gray-500'></li>
-              <li className='text-normal text-gray-500'></li>
-            </ul>
-          </p>
-        </div>
-
-        <div className='lg:w-[49%] md:w-full rounded-lg shadow border md:mt-0 xl:p-0 border-brand-primary mx-[0.5%] my-2'>
-          <h1 className='text-xl font-bold leading-tight tracking-tight md:text-2xl text-brand-primary p-2'></h1>
-          <p className='p-2'>
-            <ul className='space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400'>
-              <li className='text-normal text-gray-500'></li>
-              <li className='text-normal text-gray-500'></li>
+              <li className='text-normal text-gray-500'>
+                Tailor stokvel plans to fit specific goals, such as savings,
+                investments, or bulk purchasing.
+              </li>
+              <li className='text-normal text-gray-500'>
+                Collaborate with financial partners to explore additional
+                investment opportunities.
+              </li>
             </ul>
           </p>
         </div>
